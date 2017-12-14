eb04656f2516e76e4662641850a38bcadbf8b88a
Colin Decomis <defdruil@gmail.com>
2017-12-14 18:25:50 +0100

    Bootstrap et interface de base OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { HeaderComponent } from './Components/Common/header/header.component';
+import { FooterComponent } from './Components/Common/footer/footer.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
