62fbd336e9962f2b7921c0e2468d1487606932a5
Mat-kun <giorgiutti.mathieu@gmail.com>
2017-12-14 18:27:38 +0100

    ajout ingredient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyFridgeComponent } from './content/my-fridge/my-fridge.component';
+import { FakeFridgeService } from './services/implementations/fake-fridge.service';
+import { FridgeService } from './services/interfaces/fridge-service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFridgeComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: FridgeService, useClass: FakeFridgeService}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
